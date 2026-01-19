71c40ad002882ee4c6ac6b4315b0c61d1491ba35
Mustafa Petlad <creativegraphics2752@gmail.com>
2026-01-19 12:59:19 +0530

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountID = 123456;
+let accountEmail = "mustafa@example.com";
+var accountPassword = "12345";
+accountcity = "Mumbai"
+let accountState;
+
+
+
+// accountID = 2 // not allowed
+
+accountEmail = "hc@.com"
+accountPassword = "121212"
+accountcity = "Delhi"
+accountState;
+
+
+
+
+console.log(accountID);
+
+console.table ([accountID, accountEmail, accountPassword, accountcity])
+
