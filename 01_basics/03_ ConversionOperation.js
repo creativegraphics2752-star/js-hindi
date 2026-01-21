b00f35533642ff6133482254a874ffc782282cb2
Mustafa Petlad <creativegraphics2752@gmail.com>
2026-01-21 12:33:10 +0530

    ConversionOperation

---
+++ 01_basics/03_ ConversionOperation.js
@@ -0,0 +1,32 @@
+let digit = "Mustafa"
+console.log(typeof digit); // "string"
+console.log(typeof (digit)); // "number"
+
+let ValueInNumber = Number (digit);
+console.log(ValueInNumber);
+
+
+let somenumber = undefined;
+let sentence = 35
+console.log(typeof somenumber); // "string"node
+
+console.log(typeof sentence); 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// undefined => NaN
+// null => 0
+
+let isLoggedIn = "Mustafa";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "hello" => true
+// "" => false
+// null => false
+// undefined => false
+// NaN => false 
